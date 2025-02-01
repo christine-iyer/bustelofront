dc6351a29dbbaccbd7a408bf0478243b533f4f7f
chrisIyer <hellohotbox420@gmail.com>
2025-02-01 15:48:06 -0500

    tossed some files.

--- app/components/placeholderscreens/PlaceHolderOne.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function PlaceholderOne() {
-  return (
-    <View>
-      <Text>Placeholder Screen One</Text>
-    </View>
-  );
-}
--- app/components/placeholderscreens/PlaceHolderTwo.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function PlaceholderTwo() {
-  return (
-    <View>
-      <Text>Placeholder Screen Two</Text>
-    </View>
-  );
-}
